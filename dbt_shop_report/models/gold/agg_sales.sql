ded5fa1375a21751fe874fcafaf32efef5a8f02d
DOX69 <ggrftm@gmail.com>
2026-01-02 11:25:09 +0100

    create agg_sales

---
+++ dbt_shop_report/models/gold/agg_sales.sql
@@ -0,0 +1,9 @@
+SELECT 
+    payment_method,
+    product_name,
+    round(SUM(net_amount),2) AS total_sales_net_amount
+FROM 
+    {{ source('silver', 'sales') }}
+GROUP BY ROLLUP (
+    payment_method,
+    product_name)
